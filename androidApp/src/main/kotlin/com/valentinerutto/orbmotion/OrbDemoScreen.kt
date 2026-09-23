01dd36b612f8425b0298696534746fdfa39f2a15
valentinerutto <valentinrutto@gmail.com>
2026-09-23 13:25:31 +0300

    Update `MainActivity` and introduce `Platform` interface
    - Toggle default screen in `MainActivity` to `DemoOrbScreen`
    - Remove unused `MaterialTheme` import in `OrbDemoScreen.kt`
    - Add `Platform` interface and `getPlatform` expected function to the `shared` module for multiplatform support

--- androidApp/src/main/kotlin/com/valentinerutto/orbmotion/OrbDemoScreen.kt
+++ androidApp/src/main/kotlin/com/valentinerutto/orbmotion/OrbDemoScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
